8bb16b7002cfaea62be213520e515beeb63be287
RORY MCCONNELL <skunkbentley@hotmail.com>
2023-09-21 13:51:46 +0100

    trying before pair programming

--- app.js
+++ app.js
@@ -16,6 +16,19 @@ function greet() {
   alert("Hello " + username + ".  How delightful to have you here today");
 }
 greet();
+
+let q6Ans = ["crash", "bang", "wallop"];
+let q6 = prompt(
+  "I listed 10 of my favorite words, but, sorry, I tricked you, I only like 3 words on that list, can you guess just 1 of them?, Ill give you 6 trys to get it right"
+).toLowerCase();
+
+for (let i = 0; i < q6Ans.length; i++) {
+  if (q6 === q6Ans[i]) {
+    alert("Yes! Well done you");
+  } else q6 !== q6Ans[i];
+  prompt("please try again");
+}
+
 console.log(greet);
 
 function mybtn1() {
@@ -23,11 +36,13 @@ function mybtn1() {
 }
 
 function mybtn2() {
+  let userPoints = 0;
   let q1 = prompt("Was I born in Derry? yes/no").toLowerCase();
 
   if (q1 === "yes") {
     console.log("Correct, good start!");
     alert("Correct, good start!");
+    userPoints++;
   } else {
     console.log("oh dear, were you not paying attention?");
     alert("oh dear, were you not paying attention?");
@@ -39,6 +54,7 @@ function mybtn2() {
   if (q2 === "yes") {
     console.log("Course you did, twas a BANGER!!");
     alert("Course you did, twas a BANGER!!");
+    userPoints++;
   } else {
     console.log("Errr, are you blind?!");
     alert("Errr, are you blind?!");
@@ -50,6 +66,7 @@ function mybtn2() {
   if (q3 === "london") {
     console.log("Yes, that's right, well done smarty pants!");
     alert("Yes, that's right, well done smarty pants!");
+    userPoints++;
   } else {
     console.log("Come on now, are you even trying?");
     alert("Come on now, are you even trying?");
@@ -61,6 +78,7 @@ function mybtn2() {
   if (q4 === "dolly parton") {
     console.log("Correct, what a way to make a living eh?");
     alert("Correct, what a way to make a living eh?");
+    userPoints++;
   } else {
     console.log("no, no, NOOOOO!");
     alert("no, no, NOOOOO!");
@@ -74,16 +92,11 @@ function mybtn2() {
   if (q5 === "balding") {
     console.log("Yeah, course right, I'm practically Samson");
     alert("Yeah, course right, I'm practically Samson");
+    userPoints++;
   } else {
     console.log("How very dare you, we're gonna fall out if you keep this up");
     alert("How very dare you, we're gonna fall out if you keep this up");
   }
+  alert("You got " + userPoints + "/1 questions correct");
   console.log(q5);
 }
-
-// switch case is like another way of writing if else statements
-// if (color === 'red') {
-//     console.log('fav was red')
-// } else if (color === 'blue') {
-//     console.log('Your fav is blue!')
-// }
